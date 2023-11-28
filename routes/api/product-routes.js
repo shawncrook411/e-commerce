9deed4de079308d8ef814d752a8ba0dda7c33b16
Shawn Crook <shawncrook411@gmail.com>
2023-11-28 16:52:09 -0700

    finished project

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -48,7 +48,8 @@ router.post('/', (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      if (req.body.tagIDs) {
+        if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
@@ -56,7 +57,7 @@ router.post('/', (req, res) => {
           };
         });
         return ProductTag.bulkCreate(productTagIdArr);
-      }
+      }}
       // if no product tags, just respond
       res.status(200).json(product);
     })
